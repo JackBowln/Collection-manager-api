878a8be8d4eb5ba0c4e7acd421a3f6d5369c6409
JackBowln <diasv3861@gmail.com>
2021-02-17 23:02:23 -0300

    first version

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->group(['prefix' => 'v1'], function () use ($router) {
+  $router->get('books',  ['uses' => 'BookController@showAllBooks']);
+
+  $router->get('books/{id}', ['uses' => 'BookController@showOneBook']);
+
+  $router->post('books', ['uses' => 'BookController@create']);
+
+  $router->delete('books/{id}', ['uses' => 'BookController@delete']);
+
+  $router->put('books/{id}', ['uses' => 'BookController@update']);
+});
